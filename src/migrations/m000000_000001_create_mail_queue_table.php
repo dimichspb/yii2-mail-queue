464aacb75dff76bad0fe9133935aca0c382d7125
Dmitry Tarantin <dimichspb@gmail.com>
2018-03-01 15:12:13 +0300

    Binary...

--- src/migrations/m000000_000001_create_mail_queue_table.php
+++ src/migrations/m000000_000001_create_mail_queue_table.php
@@ -18,7 +18,7 @@ class m000000_000001_create_mail_queue_table extends Migration
             'created_at' => $this->dateTime(),
             'send_at' => $this->dateTime(),
             'attempts' => $this->text(),
-            'message_data' => $this->text(),
+            'message_data' => $this->binary(),
             'statuses' => $this->text(),
         ]);
         if ($this->db->driverName !== 'sqlite') {
